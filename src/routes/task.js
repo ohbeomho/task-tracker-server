e5a3bac3f2b3d9240b858e22ca31bbb163fdaf15
ohbeomho <ohbeomho0322@gmail.com>
2026-01-19 20:07:38 +0900

    comment

--- src/routes/task.js
+++ src/routes/task.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const taskController = require("../controllers/task");
 
-// Get all tasks
+// Get all tasks from space with spaceId
 router.get("/space/:spaceId", taskController.getSpaceTasks);
 
 // Create a new task
